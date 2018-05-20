5adfbfd302c821071e078aaafeefa3b49452f0eb
Nnamdi Affia <thegrandwizard@pc-65-76.customer.westgate.vita.lan>
2018-05-20 11:47:23 +0100

    first commit

---
+++ TTS/TTS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TTS
+//
+//  Created by Nnamdi Affia on 20/05/2018.
+//  Copyright © 2018 Nnamdi Affia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }// first commit
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
